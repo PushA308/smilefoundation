2f0e5ae300d196c3b4165d5664e5646107783942
AtramPS <piyush.atram@tatadigital.com>
2021-06-09 16:21:51 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+
+      $(document).ready(function(){
+        $('.menu-hamburger').click(function(){
+          $('.menu-hamburger').toggleClass('active')
+          $('.menu').toggleClass('active')
+        });
+      });
+
+$('.counting').each(function () {
+  var $this = $(this),
+    countTo = $this.attr('data-count');
+
+  $({ countNum: $this.text() }).animate({
+    countNum: countTo
+  },
+
+    {
+
+      duration: 3000,
+      easing: 'linear',
+      step: function () {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function () {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    });
+
+});
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
